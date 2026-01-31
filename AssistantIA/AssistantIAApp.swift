4112c42dd7194666094296c127cdca20234cac86
JB <jbsk@orange.fr>
2026-01-31 12:53:28 +0100

    Initial Commit

---
+++ AssistantIA/AssistantIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantIAApp.swift
+//  AssistantIA
+//
+//  Created by JB SENET on 31/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AssistantIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssistantIATests/AssistantIATests.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantIATests.swift
+//  AssistantIATests
+//
+//  Created by JB SENET on 31/01/2026.
+//
+
+import Testing
+@testable import AssistantIA
+
+struct AssistantIATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
